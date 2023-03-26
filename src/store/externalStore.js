87bfdfba5e5cd97c3df96240e82e96d02e4c00c4
stromcer <dannymm12@hotmail.com>
2023-03-26 03:16:41 +0200

    fix

--- src/store/externalStore.js
+++ src/store/externalStore.js
@@ -1,2 +1,3 @@
+export const AGENDA_NAME = "dmmstr"
 export const CONTACT_URL = "https://assets.breatheco.de/apis/fake/contact/";
-export const AGENDA_URL = "https://assets.breatheco.de/apis/fake/contact/agenda/dmmstr";
+export const AGENDA_URL = `https://assets.breatheco.de/apis/fake/contact/agenda/${AGENDA_NAME}`;
